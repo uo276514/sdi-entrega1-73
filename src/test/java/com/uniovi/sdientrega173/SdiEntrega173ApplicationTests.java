c2f3377d17100d2f1f7abede1ea3e97a1e6bc99b
Jorge <uo276514@uniovi.es>
2022-03-12 18:41:17 +0100

    Initial commit

---
+++ src/test/java/com/uniovi/sdientrega173/SdiEntrega173ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdientrega173;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiEntrega173ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
